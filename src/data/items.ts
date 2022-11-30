7fc27135e65eb18182b152b974847747c3a65598
FELIPE ARRUDA DE SOUZA <felipearruda@3coracoes.com.br>
2022-11-30 11:56:24 -0300

    Primeiro commit

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  {
+    date: new Date(2021, 11, 1),
+    category: "food",
+    title: "Arroz de camarão",
+    value: 80,
+  },
+  {
+    date: new Date(2021, 11, 2),
+    category: "electronics",
+    title: "Alexa",
+    value: 200,
+  },
+  {
+    date: new Date(2021, 11, 3),
+    category: "clothing",
+    title: "Calça",
+    value: 120,
+  },
+  {
+    date: new Date(2021, 11, 4),
+    category: "salary",
+    title: "Salario 3Coracoes",
+    value: 1070,
+  },
+];
